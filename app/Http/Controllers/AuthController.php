f5bc3b75c4fc7f6fd977b41c5ebd5560f50f633f
Nihaal Shaikh <nihaalshaikh06@gmail.com>
2024-03-08 09:10:00 +0100

    Laravel passport installed and login done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = Auth::user()->createToken('auth-token')->accessToken;
+            return response()->json(['token' => $token], 200);
+        }
+
+        throw ValidationException::withMessages(['error' => 'Invalid credentials']);
+    }
+}
